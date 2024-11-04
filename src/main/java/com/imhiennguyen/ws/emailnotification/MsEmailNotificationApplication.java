292168f08c41919eee67db9bd851ee39c3d75115
Hien Nguyen <nguyentronghien.contact@gmail.com>
2024-11-04 19:37:48 +0700

    feat: Init project

---
+++ src/main/java/com/imhiennguyen/ws/emailnotification/MsEmailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.imhiennguyen.ws.emailnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEmailNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imhiennguyen/ws/emailnotification/MsEmailNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imhiennguyen.ws.emailnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
